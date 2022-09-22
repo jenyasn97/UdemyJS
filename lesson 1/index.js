ec234a009b6b04017148e31a72bd440d2f3f5497
John Sultan <jenya.s97@mail.ru>
2022-09-22 16:57:10 +0300

    первая программа в курсе Ucademy "курс по JavaScript + React"

---
+++ lesson 1/index.js
@@ -0,0 +1,19 @@
+let numbersOfFilms = +prompt('Сколько фильмов вы уже посмортрели?', '');
+
+const peronalMovieDB = {
+  count: numbersOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+
+for (let i = 0; i < 2; i++) {
+  let watchedFilm = prompt("Один из последних просмотренных фильмов?");
+  let evaluation = +prompt("На сколько оценили?");
+  peronalMovieDB.movies[`${watchedFilm}`] = evaluation;
+}
+
+console.log(peronalMovieDB);
+
